0e7cf2976013d875631473bf38a70b65ab08459c
robin <robin>
2025-09-25 10:56:44 +0100

    reorg

--- update.py
+++ update.py
@@ -1,20 +1,7 @@
-import machine
+from servos import servos
 
-class Servo:
-   def __init__(self, pin):
-      self.pwm = machine.PWM(machine.Pin(pin, macine.Pin.OUT))
-      self.update_cycle(1000,2000)
-      self.angle(90)
-   def update_cycle(self,min,max):
-      self.min = min
-      self.max = max
-   def angle(self, angle):
-      self.old_angle = self.angle
-      self.angle = angle
-      new_duty_ms = self.min + int(angle * float( self.max - self.min ) / 180.0)
-      self.pwm.duty_us(new_duty_ms)
-
-servos = [Servo(p) for p in [0,1,2,3,4]]
+# servos isna dictionary of initialised class wrapped pwms
+# properties are numbers 1 to 5
 
 def reinitialize(data):
     print("reinitializing...")
