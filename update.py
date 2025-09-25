60ad31ae97de1e75222a7c5feb0b5b1b10bfcf5b
robin <robin>
2025-09-25 17:16:27 +0100

    servo_control

--- update.py
+++ update.py
@@ -6,8 +6,20 @@ from servos import servos
 def reinitialize(data):
     print("reinitializing...")
     print(data)
+    for s in servos:
+        smin = data[f"s{s}min"]
+        smax = data[f"s{s}max"]
+        sangle = data[f"s{s}"]
+        servos[s].set_limits(smin, smax)
+        servos[s].set_angle(sangle)
 
 def realtime_update(msg):
     print("updating...")
     print(msg)
-
+    key,value = msg.split(":")
+    lk = list(key)
+    s = lk[0]
+    n = int(lk[1])
+    rest = "".join(lk[2:])
+    if len(rest)==0 and s=="s":
+        servos[n].set_angle(int(value))
